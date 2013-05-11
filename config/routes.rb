cd2215684f86758ca739c83aa10d93d3eab8a26d
Michael Raimondi <zeno@pobox.com>
2013-05-10 22:44:39 -0400

    Add markup for index.html and base CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Cathub::Application.routes.draw do
+
+  resource :home
+  root :to => 'welcome#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
